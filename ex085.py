636ccf2e166f6f0fb001fb5fb061124a2705963b
MaiBV <maia.veras@gmail.com>
2024-07-19 18:27:06 -0300

    19072024 18:26

---
+++ ex085.py
@@ -0,0 +1,14 @@
+num = [[], []]
+valor = 0
+for c in range(1, 8):
+    valor = int(input('Digite um valor: '))
+    if valor % 2 == 0:
+        num[0].append(valor)
+    else:
+        num[1].append(valor)
+print('-=' * 30)
+num[0].sort()
+num[1].sort()
+print(f'Os valores pares digitados foram: {num[0]}')
+print(f'Os valores ímpares digitados foram: {num[1]}')
+
